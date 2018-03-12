9ceaf4a8de4d89c0d7cd343751bf3acd66c5c154
Muhammad Raheel <mraheel@tradetracker.com>
2018-03-12 13:22:46 +0400

    initial commit

---
+++ src/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+  "log"
+  "net/http"
+  "fmt"
+
+  "github.com/gorilla/mux"
+)
+
+func HomeEndpoint(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "Hello from TradeTracker!")
+}
+
+func VerificationEndPoint(w http.ResponseWritter, r *http.Request)  {
+  challenge := r.URL.Query().Get("hub.challenge")
+  mode := r.URL.Query().Get("hub.mode")
+  token := r.URL.Query().Get("hub.verify_token")
+
+  if mode != "" && token == os.Getenv("VERIFY_TOKEN") {
+    w.WriteHeader(200)
+    w.Write([]byte(challenge))
+  } else {
+    w.WriteHeader(400)
+    w.Write([]byte("Error", "Wrong validation token"))
+  }
+}
+
+func main() {
+  r := mux.NewRouter()
+  r.HandleFunc("/", HomeEndpoint)
+  r.HandleFunc("/webhook", VerificationEndPoint).Method("GET")
+  //r.HanleFunc("/webhook", MessagesEndPoint).Method("POST")
+  log.Fatal(http.ListenAndServe(":8080", r))
+}
