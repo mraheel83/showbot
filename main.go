0593f31f050ac7d1c4ab10dae402c83fb716081a
Muhammad Raheel <mraheel@tradetracker.com>
2018-03-13 10:10:23 +0400

    Change paths

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func HomeEndpoint(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintln(w, "Hello from TradeTracker!")
 }
 
-func VerificationEndPoint(w http.ResponseWritter, r *http.Request)  {
+/*func VerificationEndPoint(w http.ResponseWritter, r *http.Request)  {
   challenge := r.URL.Query().Get("hub.challenge")
   mode := r.URL.Query().Get("hub.mode")
   token := r.URL.Query().Get("hub.verify_token")
@@ -24,12 +24,12 @@ func VerificationEndPoint(w http.ResponseWritter, r *http.Request)  {
     w.WriteHeader(400)
     w.Write([]byte("Error", "Wrong validation token"))
   }
-}
+}*/
 
 func main() {
   r := mux.NewRouter()
   r.HandleFunc("/", HomeEndpoint)
-  r.HandleFunc("/webhook", VerificationEndPoint).Method("GET")
+  //r.HandleFunc("/webhook", VerificationEndPoint).Method("GET")
   //r.HanleFunc("/webhook", MessagesEndPoint).Method("POST")
   log.Fatal(http.ListenAndServe(":8080", r))
 }
