4a390d87d672a6bdfe5bb34dd9aa04755d0e35ca
Muhammad Raheel <mraheel@tradetracker.com>
2018-03-13 10:46:25 +0400

    New code

--- main.go
+++ main.go
@@ -1,36 +1,21 @@
-
 package main
 
-import(
-  "log"
-  "net/http"
-  "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-  "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 func HomeEndpoint(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "Hello from TradeTracker!")
+	fmt.Fprintln(w, "Hello from Raheel :)")
 }
 
-/*func VerificationEndPoint(w http.ResponseWritter, r *http.Request)  {
-  challenge := r.URL.Query().Get("hub.challenge")
-  mode := r.URL.Query().Get("hub.mode")
-  token := r.URL.Query().Get("hub.verify_token")
-
-  if mode != "" && token == os.Getenv("VERIFY_TOKEN") {
-    w.WriteHeader(200)
-    w.Write([]byte(challenge))
-  } else {
-    w.WriteHeader(400)
-    w.Write([]byte("Error", "Wrong validation token"))
-  }
-}*/
-
 func main() {
-  r := mux.NewRouter()
-  r.HandleFunc("/", HomeEndpoint)
-  //r.HandleFunc("/webhook", VerificationEndPoint).Method("GET")
-  //r.HanleFunc("/webhook", MessagesEndPoint).Method("POST")
-  log.Fatal(http.ListenAndServe(":8080", r))
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeEndpoint)
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
 }
